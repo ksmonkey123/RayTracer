f691f25d6e8aa8d64b22bf4069b46911db0503f5
Andreas Wälchli <andreas.waelchli@me.com>
2014-11-15 13:28:37 +0100

    added license header to all source files

--- src/ch/awae/ray/Color.java
+++ src/ch/awae/ray/Color.java
@@ -1,3 +1,19 @@
+/*
+ * This file is part of RayTracer.
+
+    RayTracer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    RayTracer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with RayTracer.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package ch.awae.ray;
 
 import javax.vecmath.Vector3f;
